8d569d359916ec2d1020fc22c45e567481e4a0d2
Praba Arya <106580305+aryaelmahdi@users.noreply.github.com>
2024-05-30 10:46:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ def soil_prediction():
                 }), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=int(os.environ.get("PORT", 8081)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8081)))
